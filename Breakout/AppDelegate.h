ae55ba136b1592dfbf6ecfa10f453740aab36628
Matthew Newell <matthewnewell@Matthews-Mac.local>
2014-04-13 13:44:28 -0230

    Initial Commit

---
+++ Breakout/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Breakout
+//
+//  Created by Matthew Newell on 2014-04-13.
+//  Copyright (c) 2014 mjn874@mun.ca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Breakout/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Breakout
+//
+//  Created by Matthew Newell on 2014-04-13.
+//  Copyright (c) 2014 mjn874@mun.ca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
